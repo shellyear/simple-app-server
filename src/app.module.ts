6abde44b234354bf10dd651ca5dd2f00acbf399f
NURBEK YELESH <yelesh@seznam.cz>
2023-11-20 20:09:38 +0100

    chore: structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,29 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { DatabaseModule } from './db/db.module'
+import { UsersModule } from './user/user.module'
+import { AuthModule } from './auth/auth.module'
+import Joi from 'joi'
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: {
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number()
+      }
+    }),
+    DatabaseModule,
+    UsersModule,
+    AuthModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
---
+++ src/user/user.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Get, Param, Post, Delete, UseGuards } from '@nestjs/common'
+import { CreateUserDto } from './dto/user.dto'
+import { User } from './user.entity'
+import { UserService } from './user.service'
+import { AuthGuard } from '@nestjs/passport'
+
+@Controller('user')
+export class UsersController {
+  constructor(private readonly usersService: UserService) {}
+
+  @Get(':id')
+  async getUserById(@Param('id') id: string): Promise<User> {
+    const user = await this.usersService.getUserById(Number(id))
+    return user
+  }
+
+  @Post()
+  @UseGuards(AuthGuard('jwt'))
+  async createUser(@Body() createUserDto: CreateUserDto) {
+    const newUser = await this.usersService.createUser(createUserDto)
+    return newUser
+  }
+
+  @Delete(':id')
+  async deleteById(@Param('id') id: string): Promise<User> {
+    const user = this.usersService.deleteById(Number(id))
+    return user
+  }
+}

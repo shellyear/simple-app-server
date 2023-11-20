56073ff81e57779476a8075869535982b29a5bce
NURBEK YELESH <yelesh@seznam.cz>
2023-11-20 21:03:40 +0100

    feat: add auth; controllers, jwt, passport

---
+++ src/auth/dto/signup.dto.ts
@@ -0,0 +1,20 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator'
+
+export class SignUpDto {
+  @IsNotEmpty()
+  @IsString()
+  firstName: string
+
+  @IsNotEmpty()
+  @IsString()
+  lastName: string
+
+  @IsNotEmpty()
+  @IsEmail({}, { message: 'Please enter correct email' })
+  email: string
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6, { message: 'Password must be at least 6 characters long' })
+  password: string
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,24 +6,24 @@ import { AuthGuard } from '@nestjs/passport'
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly usersService: UserService) {}
+  constructor(private readonly userService: UserService) {}
 
   @Get(':id')
   async getUserById(@Param('id') id: string): Promise<User> {
-    const user = await this.usersService.getUserById(Number(id))
+    const user = await this.userService.getUserById(Number(id))
     return user
   }
 
   @Post()
   @UseGuards(AuthGuard('jwt'))
   async createUser(@Body() createUserDto: CreateUserDto) {
-    const newUser = await this.usersService.createUser(createUserDto)
+    const newUser = await this.userService.createUser(createUserDto)
     return newUser
   }
 
   @Delete(':id')
   async deleteById(@Param('id') id: string): Promise<User> {
-    const user = this.usersService.deleteById(Number(id))
+    const user = this.userService.deleteById(Number(id))
     return user
   }
 }
